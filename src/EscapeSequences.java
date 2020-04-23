ac9949d8a4b44529aa1d1570a42a1206f4113fe1
rap9210 <rap9210@gmail.com>
2020-04-22 23:37:35 -0400

    Escape Sequences'

---
+++ src/EscapeSequences.java
@@ -0,0 +1,17 @@
+public class EscapeSequences {
+    public static void main(String[] arg){
+        System.out.println("Diamond 'Attempt'");
+        System.out.println("\t*\n  *   *\n *     *\n  *   *\n\t*");
+    }
+}
+/*
+Escape Sequence	    Description
+\t	                Insert a tab in the text at this point.
+\b	                Insert a backspace in the text at this point.
+\n	                Insert a newline in the text at this point.
+\r	                Insert a carriage return in the text at this point. Note: Windows uses \r\n for a new line.
+\f	                Insert a formfeed in the text at this point.
+\'	                Insert a single quote character in the text at this point.
+\"	                Insert a double quote character in the text at this point.
+\	                Insert a backslash character in the text at this point.
+ */
